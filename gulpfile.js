973e5c45faa5518b27f56e7c36294253f15a6771
Onur T <onurtumer@gmail.com>
2016-07-12 20:40:13 +0300

    Added slide-over, card-flip and text-animation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var gulpJade = require('gulp-jade');
+var autoprefix = require('gulp-autoprefixer');
+
 
 var del = require('del');
 var browsersync = require('browser-sync');
@@ -7,14 +9,20 @@ var browsersync = require('browser-sync');
 var src = 'src/', build = 'build/';
 
 var jade = {
-  in: src + '*.jade',
-  watch: src + '*.jade',
+  in: [src + '*.jade', src + '**/*.jade'],
+  watch: [src + '*.jade', src + '**/*.jade'],
   out: build
 };
 
 var css = {
-  in: src + '*.css',
-  watch: src + '*.css',
+  in: src + '**/*.css',
+  watch: src + '**/*.css',
+  out: build
+}
+
+var js = {
+  in: src + '**/*.js',
+  watch: src + '**/*.js',
   out: build
 }
 
@@ -34,6 +42,9 @@ gulp.task('clean', function() {
 gulp.task('css', function() {
   return gulp
     .src(css.in)
+    .pipe(autoprefix({
+      browsers:['> 5%']
+    }))
     .pipe(gulp.dest(css.out))
     .pipe(browsersync.reload({ stream: true }));
 });
@@ -46,13 +57,26 @@ gulp.task('jade', function() {
     }))
     .pipe(gulp.dest(jade.out));
 });
+// Copy JS
+gulp.task('js', function() {
+  return gulp
+    .src(js.in)
+    .pipe(gulp.dest(js.out));
+});
+// Copy images folder
+gulp.task('images', function() {
+  return gulp
+    .src(src + 'images/**.*')
+    .pipe(gulp.dest(build + 'images/'));
+});
 // BrowserSync
 gulp.task('sync', function() {
   browsersync(syncOpts);
 });
 
 // Default Task
-gulp.task('default', ['jade', 'css', 'sync'], function(){
+gulp.task('default', ['jade', 'css', 'js', 'images', 'sync'], function(){
   gulp.watch(jade.watch, ['jade', browsersync.reload]);
   gulp.watch(css.watch, ['css']);
+  gulp.watch(js.watch, ['js', browsersync.reload]);
 });
---
+++ src/slide-over/slide-over.js
@@ -0,0 +1,5 @@
+$(function() {
+  $('.nav-toggle').on('click', function() {
+    $('.main-nav').toggleClass('open');
+  });
+});
